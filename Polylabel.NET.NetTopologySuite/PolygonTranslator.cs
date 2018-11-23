2fc3c279b5b7a6aa18d20a2e8a5a7f437256efd9
Logan Dam <logan.dam@entelect.co.za>
2018-11-23 15:07:57 +0200

    fix PolygonTranslator name, used BenchmarkDotNet for benchmarks

---
+++ Polylabel.NET.NetTopologySuite/PolygonTranslator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NetTopologySuite.Geometries;
+
+namespace Polylabel.NET.NetTopologySuite
+{
+    public static class PolygonTranslator
+    {
+        public static double[][][] GetCoordinatesFromPolygon(MultiPolygon polygon)
+        {
+            return polygon.Select(p => p.Coordinates.Select(c => new[] { c.X, c.Y }).ToArray()).ToArray();
+        }
+    }
+}
