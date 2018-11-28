26c47f84951c5d11bde4ca74ba70e7cb7385e1e3
Logan Dam <logan.dam@entelect.co.za>
2018-11-28 10:50:18 +0200

    Fixed including an extra bunch of coordinates for no reason which was breaking things

--- Polylabel.NET.NetTopologySuite/PolygonTranslator.cs
+++ Polylabel.NET.NetTopologySuite/PolygonTranslator.cs
@@ -9,7 +9,7 @@ namespace Polylabel.NET.NetTopologySuite
     {
         public static double[][][] GetCoordinatesFromPolygon(MultiPolygon polygon)
         {
-            return polygon.Select(p => p.Coordinates.Select(c => new[] { c.X, c.Y }).ToArray()).ToArray();
+            return polygon.Geometries.Select(p => p.Coordinates.Select(c => new[] { c.X, c.Y }).ToArray()).ToArray();
         }
     }
 }
